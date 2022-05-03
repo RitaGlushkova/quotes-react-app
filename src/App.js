fc6d2d616d7aa2ae457e0113e6e30ce084e50c7a
Margarita Glushkova <m.glushkova1@gmail.com>
2022-05-03 21:43:32 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+// const Search = (props) => {
+//   return (
+//     <div>
+//       <h4>Search...</h4>
+//       <input
+//         onChange={props.handleSearchInput}
+//         value={props.searchInput}
+//         type="text"
+//         id="quote"
+//         placeholder="type keyword"
+//       />
+//     </div>
+//   );
+// };
 
 function App() {
+  const [quoteObject, setQuoteObject] = useState([]);
+  const [clicked, setClicked] = useState(false);
+  // const [searchInput, setSearchInput] = useState("");
+
+  // const handleSearchInput = (event) => {
+  //   setSearchInput(event.target.value);
+  // };
+  useEffect(() => {
+    fetch("https://rita-glushkova-quote-server.glitch.me/quotes/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setQuoteObject(data);
+      });
+  }, [clicked]);
+
+  // useEffect(() => {
+  //   fetch(
+  //     `https://rita-glushkova-quote-server.glitch.me/quotes/search?term=${searchInput}`
+  //   )
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //     });
+  // }, [searchInput]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div
+        className="box"
+        onClick={() => {
+          setClicked(!clicked);
+        }}
+      >
+        <i className="fas fa-quote-left fa2"></i>
+        <div className="text">
+          <i className="fas fa-quote-right fa1"></i>
+          <div>
+            <h3>Quote the day</h3>
+            <p>{quoteObject.quote}</p>
+            <cite> - {quoteObject.author}</cite>
+          </div>
+        </div>
+      </div>
+      {/* <Search handleSearchInput={handleSearchInput} searchInput={searchInput} /> */}
     </div>
   );
 }
